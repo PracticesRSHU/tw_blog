adeb1ee3682429e670359746df01848c79d3d202
Kok1ysh <yalovenko.lybomur@gmail.com>
2023-05-31 01:56:26 +0300

    Сompleted task 6

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include
-from blog import views
+from django.urls import path, re_path
+from . import views
 
-app_name = 'blog'
 
 urlpatterns = [
-
-    # path('', include('blog.urls')),
-    # path('', views.index),
-    # path('about', views.about),
-    # path('contact', views.contact),
-    path('', views.post_list, name='post_list')
+   path('post/new/', views.post_new, name='post_new'),
+   path('', views.post_list, name='post_list'),
+   re_path(r'^post/(?P<pk>[0-9]+)/$',views.post_detail, name='post_detail'),
+   re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
+
+
+# app_name = 'blog'
