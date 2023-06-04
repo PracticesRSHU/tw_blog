f6824c113335926aa4fcd0bdd9c73273c3052d26
Сергій Нездюр <serhii11mega@gmail.com>
2023-06-04 12:03:15 +0300

    add Comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,17 @@
 from django import forms
-from .models import Post
+from .models import Post, CommentPost
+
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model=Post
         fields=('title','text',)
+
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model=CommentPost
+        fields=('text',)
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from .models import Post, CommentPost
+
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request,pk):
+    post=Post.objects.get(pk=pk)
+    comment=CommentPost.objects.filter(post=post)
+    if request.method=="POST":
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comm=form.save(commit=False)
+            comm.user=request.user
+            comm.post=post
+            comm.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'posts/post_detail.html', {'post': post,'form':form,'comment':comment})
 
 
 def post_detail(request, pk):
