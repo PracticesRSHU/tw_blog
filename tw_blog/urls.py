d3db6abfbaf3aeb54b9be20efb2449f7617cd838
Віталій <ultionem18@gmail.com>
2023-06-04 13:23:11 +0300

    file

--- tw_blog/urls.py
+++ tw_blog/urls.py
@@ -21,5 +21,6 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('',include('post.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
